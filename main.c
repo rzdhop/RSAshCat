b9dd4ed6ccab49360c54ae5bade67453b18d7c93
SuijoART <wague@et.esiea.fr>
2023-03-04 22:06:57 +0100

    update main.c

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char *argv[]) {
 
-	printf("Hello World");
+	printf(" ____________________________________________");
+	printf("|           RSAshCat v1.0		           |");
+	printf("|____________________________________________|");
+
+	printf("\n\t>>> Your RSA file: %s\n", argv[1]);
+	printf("\tBegin of the attack ...");
+
 	return 0;
 }
