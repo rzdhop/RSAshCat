7e3e506db045f10344b9c25c3e0e13e451b0c947
SuijoART <wague@et.esiea.fr>
2023-03-04 22:19:57 +0100

    update main.c

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
-/* main.c */
+/* ------------------- import of libraries ------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* ------------------- main.c : Principal program ------------------- */
 
 int main(int argc, char *argv[]) {
+
+	FILE* rsa_file = fopen(argv[1],  "r");
 
 	printf(" ____________________________________________");
 	printf("|           RSAshCat v1.0		           |");
 	printf("|____________________________________________|");
 
-	printf("\n\t>>> Your RSA file: %s\n", argv[1]);
-	printf("\tBegin of the attack ...");
+	if (rsa_file == NULL) {
+		printf("\n\t>>> /!\ The RSA file you provide doesn't exist /!\");
+		printf("\n\t    Programm finished ...");
+		exit()
+
+	} else {
+		printf("\n\t>>> Your RSA file: %s", argv[1]);
+		printf("\n\tBegin of the attack ...");
+
+	}
 
 	return 0;
 }
