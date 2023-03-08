e137724ee02ec7f3f1f9ee7c86ac1cddfc413efa
Guillaume <wieczorek@et.esiea.fr>
2023-03-08 11:10:09 +0100

    Ajout TEST Récupération modulus et exposant

---
+++ InfoRSARecovery.c
@@ -0,0 +1,41 @@
+/* ------------------- import of libraries ------------------- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+/* ------------------- InfoRSARecovery.c : Recovery function of exponent and modulus ------------------- */
+
+int main()
+{
+    RSA *rsa = RSA_new();
+    if (!rsa) {
+        fprintf(stderr, "RSA_new() failed.\n");
+        exit(1);
+    }
+
+    FILE *f = fopen("../../assets/pubKey_64bit.pem", "r");
+    if (f == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+
+    PEM_read_RSA_PUBKEY(f, &rsa, NULL, NULL);
+    fclose(f);
+
+    const BIGNUM *modulus;
+    const BIGNUM *exponent;
+    RSA_get0_key(rsa, &modulus, &exponent, NULL);
+
+    char *modulus_str = BN_bn2hex(modulus);
+    char *exponent_str = BN_bn2hex(exponent);
+
+    printf("Modulus : %s\n", modulus_str);
+    printf("Exponent : %s\n", exponent_str);
+
+    RSA_free(rsa);
+    OPENSSL_free(modulus_str);
+    OPENSSL_free(exponent_str);
+
+    return 0;
+}
